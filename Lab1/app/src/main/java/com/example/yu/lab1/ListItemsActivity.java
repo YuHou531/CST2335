17b068fddf8d1ba4daa6aba92431d50d6c5a6910
Yu <yuhou531@gmail.com>
2016-09-23 23:28:29 -0400

    save the work

--- Lab1/app/src/main/java/com/example/yu/lab1/ListItemsActivity.java
+++ Lab1/app/src/main/java/com/example/yu/lab1/ListItemsActivity.java
@@ -5,10 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class ListItemsActivity extends AppCompatActivity {
-
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +25,37 @@ public class ListItemsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+        Log.i(ACTIVITY_NAME, "In onStar()");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 
 }
--- Lab1/app/src/main/java/com/example/yu/lab1/LoginActivity.java
+++ Lab1/app/src/main/java/com/example/yu/lab1/LoginActivity.java
@@ -1,20 +1,34 @@
 package com.example.yu.lab1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login = (Button) findViewById(R.id.btnLogin);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        login.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+            }
+        });
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +38,40 @@ public class LoginActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Log.i(ACTIVITY_NAME, "In onCreate()");
     }
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        // The activity is about to become visible.
+        Log.i(ACTIVITY_NAME, "In onStar()");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+        Log.i(ACTIVITY_NAME, "onDestroy()");
+    }
+    //Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+    //startActivity(intent);
 
 }
