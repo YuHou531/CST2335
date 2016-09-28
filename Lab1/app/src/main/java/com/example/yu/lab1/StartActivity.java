adec3c2e428edf2ee2ba36243d37745cc41254b2
Yu <yuhou531@gmail.com>
2016-09-27 23:07:33 -0400

    Finished Lab 3

--- Lab1/app/src/main/java/com/example/yu/lab1/StartActivity.java
+++ Lab1/app/src/main/java/com/example/yu/lab1/StartActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "StartActivity";
@@ -36,6 +37,14 @@ public class StartActivity extends AppCompatActivity {
         if(requestCode== Activity.RESULT_OK){
             Log.i(ACTIVITY_NAME,"passed:");
             String messagePassed = data.getStringExtra("Response");
+
+            if( messagePassed != null && messagePassed.length() > 0) {
+                CharSequence text = (CharSequence) messagePassed;
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(StartActivity.this , text, duration); //this is the ListActivity
+                toast.show(); //display your message box
+            }
         }
     }
 
